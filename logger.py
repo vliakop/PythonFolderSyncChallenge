2f535227cdb61e572de9a43c7cd7a34ca173f2ab
Vasileios Liakopoulos <vliakop@outlook.com>
2023-07-17 10:54:32 +0300

    Logger comments
    Commented logger class

--- logger.py
+++ logger.py
@@ -5,11 +5,13 @@ class Logger:
         self.logDir = logDir
         self.loggerFP = open(logDir, "w")
         
+    # Prepares the message to be written in logfile, stdout
     def log(self, action, filename):
         logMessage = filename + " " + action + " :" + str(datetime.now())
         self.loggerFP.write(logMessage + "\n")
         print(logMessage)
         
+    # Closes the opened file
     def close(self):
         loggerFP.close()
         
